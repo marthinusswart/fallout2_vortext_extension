0913ba360711ca230143598f4fa369a7e192ba0d
marthinusswart <marthinus.swart@gmail.com>
2024-04-18 22:11:09 +1000

    added ability to enable and disable the cheat mods

--- src/cheatmods.ts
+++ src/cheatmods.ts
@@ -1,38 +1,100 @@
 import path from "path";
-import { fs, log, types, util } from "vortex-api";
+import { fs, log, selectors, types, util } from "vortex-api";
 import { DEFAULT_EXECUTABLE, GAME_ID, STEAMAPP_ID } from "./common";
 
 const supportedFiles = ["ARTEMPLE.cfg", "artemple.map", "KLADWTWN.MAP", "KLADWTWN.txt"];
 
-export async function validateSupportedContent(files, gameId): Promise<types.ISupportedResult> {
-  let supported = gameId === GAME_ID;
-  log("info", "validating, gameId: " + gameId + " GAME_ID: " + GAME_ID);
+export async function enableCheatChestMod(context: types.IExtensionContext, modId: string) {
+  const api = context.api;
+  const game = await util.GameStoreHelper.findByAppId([STEAMAPP_ID]);
+  const gamePath = game.gamePath;
+  const state = api.getState();
+  const staging = selectors.installPathForGame(state, GAME_ID);
 
-  files.forEach((file) => {
-    log("info", "File: " + file);
-    if (supportedFiles.includes(file) && supported) {
-      supported = true;
-      log("info", "Supported: " + file);
-    } else {
-      supported = false;
-      log("info", "Not Supported: " + file);
-    }
-  });
+  let srcArtempleCfgFullpath = path.join(staging, modId);
+  srcArtempleCfgFullpath = path.join(srcArtempleCfgFullpath, "ARTEMPLE.cfg");
+  let tgtArtempleCfgFullpath = path.join(gamePath, "data\\maps");
+  tgtArtempleCfgFullpath = path.join(tgtArtempleCfgFullpath, "ARTEMPLE.cfg");
 
-  log("info", "IsSupported: " + supported);
+  let srcArtempleMapFullpath = path.join(staging, modId);
+  srcArtempleMapFullpath = path.join(srcArtempleMapFullpath, "artemple.map");
+  let tgtArtempleMapFullpath = path.join(gamePath, "data\\maps");
+  tgtArtempleMapFullpath = path.join(tgtArtempleMapFullpath, "artemple.map");
 
-  return { supported, requiredFiles: [] };
+  fs.linkSync(srcArtempleCfgFullpath, tgtArtempleCfgFullpath);
+  fs.linkSync(srcArtempleMapFullpath, tgtArtempleMapFullpath);
 }
 
-export function installContent(files) {
-  /*
-  const modFile = files.find((file) => path.extname(file).toLowerCase() === MOD_FILE_EXT);
+export async function enableStartChestMod(context: types.IExtensionContext, modId: string) {
+  const api = context.api;
+  const game = await util.GameStoreHelper.findByAppId([STEAMAPP_ID]);
+  const gamePath = game.gamePath;
+  const state = api.getState();
+  const staging = selectors.installPathForGame(state, GAME_ID);
+
+  let srcKLADWTWNTxtFullpath = path.join(staging, modId);
+  srcKLADWTWNTxtFullpath = path.join(srcKLADWTWNTxtFullpath, "Fallout 2 Start Chest");
+  srcKLADWTWNTxtFullpath = path.join(srcKLADWTWNTxtFullpath, "KLADWTWN.txt");
+  let tgtKLADWTWNTxtFullpath = path.join(gamePath, "data\\maps");
+  tgtKLADWTWNTxtFullpath = path.join(tgtKLADWTWNTxtFullpath, "KLADWTWN.txt");
+
+  let srcKLADWTWNMapFullpath = path.join(staging, modId);
+  srcKLADWTWNMapFullpath = path.join(srcKLADWTWNMapFullpath, "Fallout 2 Start Chest");
+  srcKLADWTWNMapFullpath = path.join(srcKLADWTWNMapFullpath, "KLADWTWN.MAP");
+  let tgtKLADWTWNMapFullpath = path.join(gamePath, "data\\maps");
+  tgtKLADWTWNMapFullpath = path.join(tgtKLADWTWNMapFullpath, "KLADWTWN.MAP");
+
+  fs.linkSync(srcKLADWTWNTxtFullpath, tgtKLADWTWNTxtFullpath);
+  fs.linkSync(srcKLADWTWNMapFullpath, tgtKLADWTWNMapFullpath);
+}
+
+export async function disableCheatChestMod(context: types.IExtensionContext, modId: string) {
+  const api = context.api;
+  const game = await util.GameStoreHelper.findByAppId([STEAMAPP_ID]);
+  const gamePath = game.gamePath;
+  const state = api.getState();
+  const staging = selectors.installPathForGame(state, GAME_ID);
+
+  let tgtArtempleCfgFullpath = path.join(gamePath, "data\\maps");
+  tgtArtempleCfgFullpath = path.join(tgtArtempleCfgFullpath, "ARTEMPLE.cfg");
+
+  let tgtArtempleMapFullpath = path.join(gamePath, "data\\maps");
+  tgtArtempleMapFullpath = path.join(tgtArtempleMapFullpath, "artemple.map");
+
+  fs.removeSync(tgtArtempleCfgFullpath);
+  fs.removeSync(tgtArtempleMapFullpath);
+}
+
+export async function disableStartChestMod(context: types.IExtensionContext, modId: string) {
+  const game = await util.GameStoreHelper.findByAppId([STEAMAPP_ID]);
+  const gamePath = game.gamePath;
+
+  let tgtKLADWTWNTxtFullpath = path.join(gamePath, "data\\maps");
+  tgtKLADWTWNTxtFullpath = path.join(tgtKLADWTWNTxtFullpath, "KLADWTWN.txt");
+
+  let tgtKLADWTWNMapFullpath = path.join(gamePath, "data\\maps");
+  tgtKLADWTWNMapFullpath = path.join(tgtKLADWTWNMapFullpath, "KLADWTWN.MAP");
+
+  fs.removeSync(tgtKLADWTWNTxtFullpath);
+  fs.removeSync(tgtKLADWTWNMapFullpath);
+}
+
+export async function validateSupportedContent(files, gameId): Promise<types.ISupportedResult> {
+  const supported = gameId === GAME_ID;
+  log("info", "validating, gameId: " + gameId + " GAME_ID: " + GAME_ID);
+  return { supported: false, requiredFiles: [] };
+}
+
+export async function installContent(files) {
+  alert("Checking installed files");
+
+  const modFile = files.find((file) => path.extname(file).toLowerCase() === ".MAP");
   const idx = modFile.indexOf(path.basename(modFile));
   const rootPath = path.dirname(modFile);
 
   // Remove directories and anything that isn't in the rootPath.
   const filtered = files.filter((file) => file.indexOf(rootPath) !== -1 && !file.endsWith(path.sep));
-  
+
   const instructions = filtered.map((file) => {
     return {
       type: "copy",
@@ -40,7 +102,6 @@ export function installContent(files) {
       destination: path.join(file.substr(idx)),
     };
   });
-*/
-  //return Promise.resolve({ instructions });
-  return true;
+
+  return { instructions };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { fs, log, types, util } from "vortex-api";
-import { installContent, validateSupportedContent } from "./cheatmods";
+import { disableCheatChestMod, disableStartChestMod, enableCheatChestMod, enableStartChestMod } from "./cheatmods";
 import { DEFAULT_EXECUTABLE, GAME_ID, STEAMAPP_ID } from "./common";
 
 function findGame() {
@@ -11,11 +11,21 @@ function prepareForModding(discovery) {
   return fs.ensureDirWritableAsync(path.join(discovery.path, "backups"));
 }
 
+function modEnabledCallback(context: types.IExtensionContext, profileId: string, modId: string) {
+  if (modId === "Cheat Chest 1.0-15-1-0") enableCheatChestMod(context, modId);
+  if (modId === "Leather Armor Basic Kit-27-1-1-1585807325") enableStartChestMod(context, modId);
+}
+
+function modDisabledCallback(context: types.IExtensionContext, profileId: string, modId: string) {
+  if (modId === "Cheat Chest 1.0-15-1-0") disableCheatChestMod(context, modId);
+  if (modId === "Leather Armor Basic Kit-27-1-1-1585807325") disableStartChestMod(context, modId);
+}
+
 function main(context: types.IExtensionContext) {
   context.registerGame({
     id: GAME_ID,
     name: "Fallout 2",
-    mergeMods: true,
+    mergeMods: false,
     queryPath: findGame,
     supportedTools: [],
     queryModPath: () => ".",
@@ -31,8 +41,8 @@ function main(context: types.IExtensionContext) {
     },
   });
 
-  context.registerInstaller("fallout2-cheat-mods", 25, validateSupportedContent as any, installContent);
-  //context.registerInstaller("fallout2-cheat-mods", 25, testSupportedContent, installContent2);
+  context.api.events.on("mod-enabled", async (profileId, modId) => modEnabledCallback(context, profileId, modId));
+  context.api.events.on("mod-disabled", async (profileId, modId) => modDisabledCallback(context, profileId, modId));
 
   return true;
 }
